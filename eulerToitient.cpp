14d9052334c91a85229482411dda17784285c043
Sreya Maitra <maitrasreya02@gmail.com>
2024-10-06 22:36:23 +0530

    eulerToitient

---
+++ eulerToitient.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int euler(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0) {
+                n /= i;
+            }
+            result -= result / i; 
+        }
+    }
+    if (n > 1) {
+        result -= result / n;
+    }
+    return result;
+}
